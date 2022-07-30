b3d39a2cf7585d07fad1d57712a0c669d84f907d
eo39 <zhekaonichsenko@gmail.com>
2022-07-30 16:21:56 +0200

    - Moved event handlers to separate part of class. - Added new event handlers.

---
+++ DiscordBot/DiscordBot/DiscordBot.EventHandlers.cs
@@ -0,0 +1,31 @@
+﻿namespace DiscordBot;
+
+public partial class DiscordBot
+{
+    private Task OnLog(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+    
+    private Task OnMessageReceived(SocketMessage message)
+    {
+        if (message.Author.IsBot)
+            return Task.CompletedTask;
+
+        message.Channel.SendMessageAsync($"Hey, {message.Author.Mention}!").ConfigureAwait(false);
+        return Task.CompletedTask;
+    }
+
+    private Task OnGuildAvailable(SocketGuild guild)
+    {
+        this.guildInfos[guild.Id] = guild;
+
+        return Task.CompletedTask;
+    }
+
+    private Task OnReactionAdded(Cacheable<IUserMessage, ulong> message, Cacheable<IMessageChannel, ulong> channel, SocketReaction reaction)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ DiscordBot/DiscordBot/DiscordBot.cs
@@ -0,0 +1,37 @@
+﻿namespace DiscordBot;
+
+using System.Collections.Concurrent;
+
+public partial class DiscordBot
+{
+    private readonly DiscordSocketClient client;
+    private readonly IDictionary<ulong, SocketGuild> guildInfos;
+    private Config? config;
+
+    public DiscordBot()
+    {
+        this.client     = new DiscordSocketClient();
+        this.guildInfos = new ConcurrentDictionary<ulong, SocketGuild>();
+    }
+
+    public async Task Initialize()
+    {
+        try
+        {
+            string appSettings = await File.ReadAllTextAsync("appsettings.json").ConfigureAwait(false);
+            this.config = JsonConvert.DeserializeObject<Config>(appSettings);
+
+            this.client.Log             += this.OnLog;
+            this.client.GuildAvailable  += this.OnGuildAvailable;
+            this.client.MessageReceived += this.OnMessageReceived;
+            this.client.ReactionAdded   += this.OnReactionAdded;
+
+            await this.client.LoginAsync(TokenType.Bot, this.config?.Token).ConfigureAwait(false);
+            await this.client.StartAsync().ConfigureAwait(false);;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Initialize error!");
+        }
+    }
+}
